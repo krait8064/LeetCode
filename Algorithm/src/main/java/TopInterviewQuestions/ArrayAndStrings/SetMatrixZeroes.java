7f2553cecf04a9a112d4538fd604bfe6a1127f15
krait8064 <krait8064@163.com>
2018-06-12 07:05:56 +0800

    init-setMatrixZero debugged ok

--- Algorithm/src/main/java/TopInterviewQuestions/ArrayAndStrings/SetMatrixZeroes.java
+++ Algorithm/src/main/java/TopInterviewQuestions/ArrayAndStrings/SetMatrixZeroes.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class SetMatrixZeroes {
     public static void main(String[] args) {
-        int[][] arr = {{1, 0, 2}};
-        System.out.println(Arrays.toString(arr[0]));
+        int[][] arr = {{1, 0, 2}, {2, 1, 3}, {1, 4, 0}};
+        setZeroes(arr);
+        System.out.println(convert2Arr(arr));
     }
 
     public static void setZeroes(int[][] arr) {
@@ -13,12 +14,19 @@ public class SetMatrixZeroes {
             return;
         int row = arr.length;
         int col = arr[0].length;
-        int[] extras = new int[row + col];
-        for (int i = 0; i < row + col; i++) {
-            if ((i < row && arr[i][0] == 0) || (i >= row && arr[0][i - row] == 0))
-                extras[i] = 0;
-            else
-                extras[i] = -1;
+        boolean rowZ = false;
+        boolean colZ = false;
+        for (int i = 0; i < row; i++) {
+            if (arr[i][0] == 0) {
+                colZ = true;
+                break;
+            }
+        }
+        for (int j = 0; j < col; j++) {
+            if (arr[0][j] == 0) {
+                rowZ = true;
+                break;
+            }
         }
 
         for (int i = 1; i < arr.length; i++) {
@@ -30,21 +38,19 @@ public class SetMatrixZeroes {
             }
         }
 
-        for (int i = 1; i < arr[0].length; i++) {
+        for (int i = 0; i < arr[0].length; i++) {
             if (arr[0][i] == 0)
                 set(arr, false, i);
         }
 
-        for (int j = 1; j < arr.length; j++) {
+        for (int j = 0; j < arr.length; j++) {
             if (arr[j][0] == 0)
                 set(arr, true, j);
         }
-        for (int i = 0; i < extras.length; i++) {
-            if (i < row && extras[i] == 0)
-                set(arr, true, i);
-            if (i >= row && extras[i] == 0)
-                set(arr, false, i - row);
-        }
+        if (rowZ)
+            set(arr, true, 0);
+        if (colZ)
+            set(arr, false, 0);
 
     }
 
@@ -58,6 +64,14 @@ public class SetMatrixZeroes {
                 arr[i][index] = 0;
             }
         }
+    }
+
+    private static String convert2Arr(int[][] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(Arrays.toString(arr[i]) + "\n");
+        }
+        return sb.toString();
     }
 
 }
