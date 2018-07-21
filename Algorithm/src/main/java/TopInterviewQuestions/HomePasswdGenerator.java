ccb71ea0a41741bdd0ab751b127331125cfd6529
krait8064 <krait8064@163.com>
2018-07-21 14:42:22 +0800

    pwd generator

---
+++ Algorithm/src/main/java/TopInterviewQuestions/HomePasswdGenerator.java
@@ -0,0 +1,22 @@
+package TopInterviewQuestions;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class HomePasswdGenerator {
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(generate(10,6)));
+    }
+
+    public static int[] generate(int bound, int bits) {
+        int[] arr = new int[bits];
+        for ( int i = 0; i< bits; i++ ) {
+            arr[i] = random.nextInt(bound);
+        }
+        return arr;
+    }
+
+}
+
